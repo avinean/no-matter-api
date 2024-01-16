c77c6d6ecfb37d1147c1feae3fc7a5f371c39645
Vladyslav Lubeznyy <vladyslavlubeznyy@MacBook-Pro-Vlad.local>
2024-01-16 09:17:43 +0200

    update materials transactions routing

--- src/controllers/materials/materials.controller.ts
+++ src/controllers/materials/materials.controller.ts
@@ -13,52 +13,52 @@ import { Roles } from 'src/decorators/role.decorator';
 import { Role } from 'src/types/role';
 import { Public } from 'src/decorators/public.decorator';
 
-@Controller('')
+@Controller('materials')
 export class MaterialsController {
   constructor(private readonly materialsService: MaterialsService) {}
 
-  @Get('materials')
+  @Public()
+  @Get('transactions')
+  findAllTransactions() {
+    return this.materialsService.findAllTransactions();
+  }
+
+  @Get('transactions/:id')
+  findOneTransaction(@Param('id') id: number) {
+    return this.materialsService.findOneTransaction(id);
+  }
+
+  @Post('transactions')
+  addTransaction(@Body() dto: CreateTransactionDto) {
+    return this.materialsService.createTransaction(dto);
+  }
+
+  @Get('')
   findAll() {
     return this.materialsService.findAll();
   }
 
   @Roles([Role.Admin, Role.Owner])
-  @Get('materials/:id')
+  @Get(':id')
   findOne(@Param('id') id: number) {
     return this.materialsService.findOne(id);
   }
 
   @Roles([Role.Admin, Role.Owner])
-  @Post('materials')
+  @Post('')
   create(@Body() dto: CreateMaterialDto) {
     return this.materialsService.create(dto);
   }
 
   @Roles([Role.Admin, Role.Owner])
-  @Put('materials/:id')
+  @Put(':id')
   update(@Param('id') id: number, @Body() dto: CreateMaterialDto) {
     return this.materialsService.update(id, dto);
   }
 
   @Roles([Role.Admin, Role.Owner])
-  @Delete('materials/:id')
+  @Delete(':id')
   remove(@Param() id: number) {
     return this.materialsService.remove(id);
-  }
-
-  @Public()
-  @Get('materials-transactions')
-  findAllTransactions() {
-    return this.materialsService.findAllTransactions();
-  }
-
-  @Get('materials-transactions/:id')
-  findOneTransaction(@Param('id') id: number) {
-    return this.materialsService.findOneTransaction(id);
-  }
-
-  @Post('materials-transactions')
-  addTransaction(@Body() dto: CreateTransactionDto) {
-    return this.materialsService.createTransaction(dto);
   }
 }
